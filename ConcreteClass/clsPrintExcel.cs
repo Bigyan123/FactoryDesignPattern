29633375b442b91a39672c29274a86dc6c619b2b
Bigyan123 <bigyanb5@gmail.com>
2021-06-20 01:39:28 +0530

    Add files via upload

---
+++ ConcreteClass/clsPrintExcel.cs
@@ -0,0 +1,15 @@
+﻿using PrintInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcreteClass
+{
+    public class clsPrintExcel : IPrint
+    {
+        public void Print()
+        {
+            Console.WriteLine("Print Excel Data");
+        }
+    }
+}
---
+++ ConcreteClass/clsPrintPDF.cs
@@ -0,0 +1,13 @@
+﻿using PrintInterface;
+using System;
+
+namespace ConcreteClass
+{
+    public class clsPrintPDF : IPrint
+    {
+        public void Print()
+        {
+            Console.WriteLine("Print PDF Data");
+        }
+    }
+}
